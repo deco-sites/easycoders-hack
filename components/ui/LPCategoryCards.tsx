cf1d4fda19eb2c041439979e0720eb7fbe3adf08
NetinhoSZ <netinhoskennedy@gmail.com>
2023-10-31 23:15:43 -0300

    Fix: Fixing margin Section LPCategoryCards.tsx

--- components/ui/LPCategoryCards.tsx
+++ components/ui/LPCategoryCards.tsx
@@ -1,3 +1,4 @@
+import Image from "apps/website/components/Image.tsx";
 import type { HTMLWidget } from "apps/admin/widgets.ts";
 import type { ImageWidget } from "apps/admin/widgets.ts"; 
 
@@ -5,6 +6,7 @@ export interface Props{
     title?: HTMLWidget;
     subTitle?: HTMLWidget;
     cards?: {
+        link: string;
         image: {
             link: ImageWidget;
             description: string;
@@ -16,21 +18,20 @@ export interface Props{
 
 export default function LPCategoryCards( {title, subTitle, cards}: Props ) {
     return(
-        <div class="w-full h-full bg-black">
+        <div class="w-full h-[420px] bg-black">
             <section class="w-full h-full flex flex-col items-center justify-center">
                 <div class="w-full text-center mt-4">
                     <h1 class="text-2xl text-white font-bold"
-                    dangerouslySetInnerHTML={{ __html: title}}/>
+                    dangerouslySetInnerHTML={{ __html: title || ""}}/>
                     <p class="text-sm text-white"
-                    dangerouslySetInnerHTML={{ __html: subTitle}}/>
-                    
+                    dangerouslySetInnerHTML={{ __html: subTitle || ""}}/>
                 </div>
                 <div class="w-full h-full flex flex-wrap items-center justify-center my-4 gap-4">
                     {cards?.map((item) => (
-                        <div class="flex flex-col items-center justify-center">
-                            <img src={item.image.link} alt="Imagem" width={140} height={150} class="rounded-lg w-[140px] h-[150px]" alt={item.image.description}/>
-                            <p class="w-[130px] h-full text-lg text-center text-white font-semibold" >{item.titleCategory}</p>
-                        </div>
+                        <a href={item.link} class="flex flex-col items-center justify-center">
+                            <Image src={item.image.link} width={191} height={191} class="rounded-lg" alt={item.image.description}/>
+                            <p class="w-[130px] min-h-[58px] text-lg text-center text-white font-semibold" >{item.titleCategory}</p>
+                        </a>
                     ))}
                 </div>
             </section>
